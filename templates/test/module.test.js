d7c63517bb207a0cd0061990fe17e2ccfdda5baf
Alexander Wende <alex.wende@gmail.com>
2014-10-22 18:06:17 +0200

    initial commit

---
+++ templates/test/module.test.js
@@ -0,0 +1,35 @@
+var System = require('systemjs');
+var assert = require('chai').assert;
+
+System.config({
+    baseURL: 'src/js'
+});
+
+describe('<%= moduleExport %>', function () {
+
+    'use strict';
+
+    var <%= moduleExport %>;
+
+    before(function (done) {
+
+        System.import('<%= modulePath %>')
+            .then(function (module) {
+                <%= moduleExport %> = module.default;
+                done();
+            })
+            .catch(function (error) {
+                throw error;
+            });
+    });
+
+    describe('#someMethod()', function () {
+
+        it('should do something', function (done) {
+
+            // assert something
+
+            done();
+        });
+    });
+});
